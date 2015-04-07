085c3e2bf0392504c6ec77e311fec7ac8b8104b6
David Kleinjan <kleinjan@lanl.gov>
2015-04-07 09:31:27 -0600

    first commit

---
+++ sql2root/getkdimuon.C
@@ -0,0 +1,234 @@
+void getkdimuon(const char *schema, const int tgtdmp = 2)
+{
+  //connect to server
+  //TSQLServer *con = TSQLServer::Connect("mysql://seaquel.physics.illinois.edu:3283", "seaguest", "qqbar2mu+mu-");
+  TSQLServer *con = TSQLServer::Connect("mysql://localhost:3306", "root", "");
+  
+  //TTreeSQL * treeql_run = new TTreeSQL(con, schema, "mRun");
+  Int_t nEvGen; 
+  Int_t nevgen = 1;
+  Float_t sigwt =1;
+
+  cout << nevgen << endl;
+  
+  TTreeSQL * treeql = new TTreeSQL(con, schema, "kDimuon");
+  Int_t dimuonID;
+  Int_t runID;
+  Int_t spillID, eventID;
+  UInt_t negTrack, posTrackID;
+  Float_t xF, xT, xB, mass;
+  Float_t dx, dy, dz;
+  Float_t dpx, dpy, dpz;
+  Float_t px1, py1, pz1;
+  Float_t px2, py2, pz2;
+  Float_t trackSeparation, chisq_dimuon;
+  UInt_t target, dump;
+  UInt_t targetPos;
+  Float_t m3hm, m3hs, m3vm, m3vs;
+  Float_t m2hm, m2hs, m2vm, m2vs;
+
+
+  TH1F *Hmass = new TH1F("Hmass", "HodoAll Mass of mu+mu- pair", 100, 0, 10);
+  TH1F *Hpz = new TH1F("Hpz", "HodoAll pz of mu+mu- pair", 120,0,120);
+  TH1F *HxF = new TH1F("HxF", "HodoAll Feynman-x of pair", 100, -1, 1);
+  TH1F *HxB = new TH1F("HxB", "HodoAll Bjorken-x of Beam, x_{B}", 100, 0, 1);
+  TH1F *HxT = new TH1F("HxT", "HodoAll Bjorken-x of Target, x_{T}", 100, 0, 1);
+
+  TH2F *HxTxB = new TH2F("HxTxB", "HodoAllx_{T} vs. x_{B}", 100, 0, 1, 100, 0, 1);
+  TH2F *Hpzmass = new TH2F("Hpzmass", "HodoAll p_{z} vs. Mass", 100, 0, 10, 120, 0, 120);
+
+  TH1F *Zvtx = new TH1F("Zvtx"," Smeared Zvtx",160,-400,240);
+  Zvtx -> Sumw2();
+  Zvtx -> SetXTitle("Zvtx [cm]");
+  Zvtx -> SetYTitle("Rate [Hz]");
+
+  Hmass -> SetXTitle("mass [GeV/c^{2}]");
+  HxF -> SetXTitle("Feynman-x");
+  HxB -> SetXTitle("x_{B}");
+  HxT -> SetXTitle("x_{T}");
+  Hpz -> SetXTitle("p_{z}");
+  HxF -> SetYTitle("Rate [Hz]");
+  HxB -> SetYTitle("Rate [Hz]");
+  HxT -> SetYTitle("Rate [Hz]");
+  Hpz -> SetYTitle("Rate [Hz]");
+  HxTxB -> SetXTitle("x_{T}");
+  HxTxB -> SetYTitle("x_{B}");
+  Hpzmass -> SetXTitle("mass [GeV/c^{2}]");
+  Hpzmass -> SetYTitle("p_{z}");
+
+  Hmass -> Sumw2();
+  HxF -> Sumw2();
+  HxB -> Sumw2();
+  HxT ->  Sumw2();
+  Hpz ->  Sumw2();
+  HxTxB ->  Sumw2();
+  Hpzmass ->  Sumw2();
+
+  //Make NTuple for Andi, Kun
+  TTree *kdimuon = new TTree("kdimuon","kdimuon");
+  kdimuon -> SetDirectory(0);
+  kdimuon -> Branch ("dimuonID",        &dimuonID,        "dimuonID/I");
+  kdimuon -> Branch ("runID",           &runID,           "runID/I");
+  kdimuon -> Branch ("eventID",         &eventID,         "eventID/I");
+  kdimuon -> Branch ("spillID",         &spillID,         "spillID/I");
+  //kdimuon -> Branch ("posTrackID",    &posTrackID       "posTrackID/I");
+  //kdimuon -> Branch ("negTrackID",    &negTrackID       "negTrackID/I");
+  kdimuon -> Branch ("dx",              &dx,              "dx/F");
+  kdimuon -> Branch ("dy",              &dy,              "dy/F");
+  kdimuon -> Branch ("dz",              &dz,              "dz/F");
+  kdimuon -> Branch ("dpx",             &dpx,             "dpx/F");
+  kdimuon -> Branch ("dpy",             &dpy,             "dpy/F");
+  kdimuon -> Branch ("dpz",             &dpz,             "dpz/F");
+  kdimuon -> Branch ("mass",            &mass,            "mass/F");
+  kdimuon -> Branch ("xF",              &xF,              "xF/F");  
+  kdimuon -> Branch ("xB",              &xB,              "xB/F");
+  kdimuon -> Branch ("xT",              &xT,              "xT/F");
+  kdimuon -> Branch ("trackSeparation", &trackSeparation, "trackSeparation/F");
+  kdimuon -> Branch ("chisq_dimuon",    &chisq_dimuon,    "chisq_dimuon/F");
+  //kdimuon -> Branch ("px1",           &px1,             "px1/F");
+  //kdimuon -> Branch ("py1",           &py1,             "py1/F");
+  //kdimuon -> Branch ("pz1",           &pz1,             "pz1/F");
+  //kdimuon -> Branch ("px2",           &px2,             "px2/F");
+  //kdimuon -> Branch ("py2",           &py2,             "py2/F");
+  //kdimuon -> Branch ("pz2",           &pz2,             "pz2/F");
+  kdimuon -> Branch ("target",          &target,          "target/I");
+  kdimuon -> Branch ("dump",            &dump,            "dump/I");
+  kdimuon -> Branch ("targetPos",       &targetPos,       "targetPos/I");
+  kdimuon -> Branch ("m3hm",            &m3hm,            "m3hm/F");
+  kdimuon -> Branch ("m3hs",            &m3hs,            "m3hs/F");
+  kdimuon -> Branch ("m3vm",            &m3vm,            "m3vm/F");
+  kdimuon -> Branch ("m3vs",            &m3vs,            "m3vs/F");
+  kdimuon -> Branch ("m2hm",            &m2hm,            "m2hm/F");
+  kdimuon -> Branch ("m2hs",            &m2hs,            "m2hs/F");
+  kdimuon -> Branch ("m2vm",            &m2vm,            "m2vm/F");
+  kdimuon -> Branch ("m2vs",            &m2vs,            "m2vs/F");
+
+  treeql -> SetBranchAddress ("dimuonID",        &dimuonID);
+  treeql -> SetBranchAddress ("runID",           &runID);
+  treeql -> SetBranchAddress ("eventID",         &eventID);
+  treeql -> SetBranchAddress ("spillID",         &spillID);
+  //treeql -> SetBranchAddress ("posTrackID",    &posTrackID);
+  //treeql -> SetBranchAddress ("negTrackID",    &negTrackID);
+  treeql -> SetBranchAddress ("dx",              &dx);
+  treeql -> SetBranchAddress ("dy",              &dy);
+  treeql -> SetBranchAddress ("dz",              &dz);
+  treeql -> SetBranchAddress ("dpx",             &dpx);
+  treeql -> SetBranchAddress ("dpy",             &dpy);
+  treeql -> SetBranchAddress ("dpz",             &dpz);
+  treeql -> SetBranchAddress ("mass",            &mass);
+  treeql -> SetBranchAddress ("xF",              &xF);
+  treeql -> SetBranchAddress ("xB",              &xB);
+  treeql -> SetBranchAddress ("xT",              &xT);
+  treeql -> SetBranchAddress ("trackSeparation", &trackSeparation);
+  treeql -> SetBranchAddress ("chisq_dimuon",    &chisq_dimuon);
+  //treeql -> SetBranchAddress ("px1",             &px1);
+  //treeql -> SetBranchAddress ("py1",             &py1);
+  //treeql -> SetBranchAddress ("pz1",             &pz1);
+  //treeql -> SetBranchAddress ("px2",             &px2);
+  //treeql -> SetBranchAddress ("py2",             &py2);
+  //treeql -> SetBranchAddress ("pz2",             &pz2);
+  treeql -> SetBranchAddress ("target",          &target);
+  treeql -> SetBranchAddress ("dump",            &dump);
+  treeql -> SetBranchAddress ("targetPos",       &targetPos);
+  treeql -> SetBranchAddress ("m3hm",            &m3hm);
+  treeql -> SetBranchAddress ("m3hs",            &m3hs);
+  treeql -> SetBranchAddress ("m3vm",            &m3vm);
+  treeql -> SetBranchAddress ("m3vs",            &m3vs);
+  treeql -> SetBranchAddress ("m2hm",            &m2hm);
+  treeql -> SetBranchAddress ("m2hs",            &m2hs);
+  treeql -> SetBranchAddress ("m2vm",            &m2vm);
+  treeql -> SetBranchAddress ("m2vs",            &m2vs);
+
+
+  int nentries = treeql -> GetEntries();
+  cout << "The number of Entries is " << nentries << endl;
+  for(int i = 0; i < nentries; i++){
+    treeql -> GetEntry(i);
+
+    if(target == 0 && dump ==0) cout << runID << " " << spillID << " " << eventID << " " << target << " " << dump << " " << targetPos << endl;
+    //cuts
+    if(m3hm<-5 || m3hm > 5)continue; 
+    if(m3vm<-5 || m3vm > 5)continue; 
+    if(m2hm<-5 || m2hm > 5)continue; 
+    if(m2vm<-5 || m2vm > 5)continue; 
+    if(mass < 4.2)continue;
+    if(tgtdmp == 0 && target == 0) continue;
+    if(tgtdmp == 1 && dump == 0) continue;
+    if(tgtdmp == 2 && dump == 0 && target == 0) continue;
+    if(tgtdmp == 3 && (dump == 1 || target == 1)) continue;
+    kdimuon -> Fill();
+
+    //if(eventID%1 == 0) cout << "EventID " << eventID << endl;
+    Hmass -> Fill(mass, sigwt);
+    Hpz -> Fill(dpz, sigwt);
+    HxF -> Fill(xF, sigwt);
+    HxT -> Fill(xT, sigwt);
+    HxB -> Fill(xB, sigwt);
+    HxTxB -> Fill(xT, xB, sigwt);
+    Hpzmass -> Fill(mass, dpz, sigwt);
+
+    dz = gRandom -> Gaus(dz,35);
+    Zvtx -> Fill(dz, sigwt);
+     
+    //TCanvas *c1 = new TCanvas("c1","c1",
+
+  }
+
+  Int_t getbin;
+  char Hname[64];
+  TH1F *HxTbins[20];
+  
+  TGraphErrors *GxT = new TGraphErrors();
+  GxT -> SetMarkerStyle(21);
+  GxT -> SetTitle("Target-x");
+  GxT -> SetName("GxT");
+  Int_t bval = 0;
+
+  Double_t x[20], y[20],ex[20],ey[20];
+
+  //get mean xT for 4-6 points
+  for(int b = 0; b < 20; b++){
+    sprintf(Hname, "HxTbins_xt%d", b);
+    HxTbins[b] = new TH1F(Hname, Hname, 100, 0, 1);
+     
+    for(int i = 1; i < 6; i++){     
+      getbin = 5*b + i;
+      HxTbins[b] -> SetBinContent(getbin, HxT -> GetBinContent(getbin));
+      HxTbins[b] -> SetBinError  (getbin, HxT -> GetBinError(getbin));       
+    }
+
+    x[b] = HxTbins[b] -> GetMean();
+    y[b] = HxTbins[b] -> IntegralAndError(1,100,ey[b]);
+
+    if(y[b] > 1.0e-9){
+      GxT -> SetPoint(bval, x[b], y[b]);
+      GxT -> SetPointError(bval, 0, ey[b]);
+      bval++;
+    }
+  }
+
+  GxT -> GetXaxis() -> SetTitle("x_{T}");
+  GxT -> GetYaxis() -> SetTitle("Rate [Hz]");
+
+  char outname[128];
+  sprintf(outname, "./nDST/%s_tgt%d.root", schema, tgtdmp);
+  TFile *outfile = new TFile(outname,"RECREATE");
+  outfile -> cd();
+  kdimuon -> Write();
+  Hmass -> Write();
+  Hpz -> Write();
+  HxF -> Write();
+  HxB -> Write();
+  HxT -> Write();
+  HxTxB -> Write();
+  Hpzmass -> Write();
+ 
+  for(int b = 0; b < 20; b++){
+    HxTbins[b] -> Write();
+  }
+
+  GxT -> Write("GxT");
+  Zvtx -> Write();
+  outfile -> Close();
+
+}
---
+++ sql2root/getktrack.C
@@ -0,0 +1,156 @@
+void getktrack(const char *schema, const int val)
+{
+
+  char stmt [1024];
+  char delstmt [64];
+  char seedstmt [64];
+  char table[64];
+  int range = 1000000;
+  int limit = val*range + range;
+
+  sprintf(table, "kTrack_%d", val);
+
+  // Assign which seed to use (this chooses a database on the server)
+  sprintf(seedstmt, "USE %s", schema);
+
+   // Drop the table if it's already there for some reason
+   sprintf(delstmt, "DROP TABLE IF EXISTS kTrack_%d", val);
+
+   sprintf(stmt, // This first part tells it where to put the data you want
+	   "CREATE TABLE kTrack_%d "
+	   "SELECT kTrack.* FROM kTrack "
+	   "LIMIT %d, %d;",val, limit, range);
+   //exit(1);
+
+   cout << stmt << endl;
+   // This it used to time the SQL action of the macro
+   gBenchmark->Start("SQL");
+
+   //connect to server
+   //TSQLServer *con = TSQLServer::Connect("mysql://seaquel.physics.illinois.edu:3283", "seaguest", "qqbar2mu+mu-");
+   TSQLServer *con = TSQLServer::Connect("mysql://localhost:3306", "root", "");
+
+   // Select which database to use on the server
+   con->Exec(seedstmt);
+
+   // Delete the table if it already exitst
+   con->Exec(delstmt);
+
+   // Create a table based on the exact cut of data you want
+   con->Exec(stmt);
+
+   // Display the time it took
+   gBenchmark->Show("SQL");
+
+   TTreeSQL * treeql = new TTreeSQL(con, schema, table);
+
+
+   Int_t trackID, runID, spillID, eventID, charge, roadID;
+   Int_t numHits, numHitsSt1, numHitsSt2, numHitsSt3, numHitsSt4H, numHitsSt4V;
+   Float_t chisq;
+   Float_t x0, y0, z0;
+   Float_t x_target, y_target, z_target;
+   Float_t x_dump, y_dump, z_dump;
+   Float_t px0, py0, pz0;
+   Float_t x1, y1, z1;
+   Float_t px1, py1, pz1;
+   Float_t x3, y3, z3;
+   Float_t px3, py3, pz3;
+   Float_t tx_PT, ty_PT;
+   UInt_t target, dump, targetPos;
+
+
+  TTree *ktrack = new TTree("ktrack","ktrack");
+  ktrack -> SetDirectory(0);
+  ktrack -> Branch ("runID",     &runID,     "runID/I");
+  ktrack -> Branch ("spillID",   &spillID,   "spillID/I");
+  ktrack -> Branch ("eventID",   &eventID,   "eventID/I");
+  ktrack -> Branch ("charge",    &charge,    "charge/I");
+  ktrack -> Branch ("roadID",    &roadID,    "roadID/I");
+  ktrack -> Branch ("chisq",     &chisq,     "chisq/F");
+  ktrack -> Branch ("x0",        &x0,        "x0/F");
+  ktrack -> Branch ("y0",        &y0,        "y0/F");
+  ktrack -> Branch ("z0",        &z0,        "z0/F");
+  ktrack -> Branch ("x_target",  &x_target,  "x_target/F");
+  ktrack -> Branch ("y_target",  &y_target,  "y_target/F");
+  ktrack -> Branch ("z_target",  &z_target,  "z_target/F");
+  ktrack -> Branch ("x_dump",    &x_dump,    "x_dump/F");
+  ktrack -> Branch ("y_dump",    &y_dump,    "y_dump/F");
+  ktrack -> Branch ("z_dump",    &z_dump,    "z_dump/F");
+  ktrack -> Branch ("x1",        &x1,        "x1/F");
+  ktrack -> Branch ("y1",        &y1,        "y1/F");
+  ktrack -> Branch ("z1",        &z1,        "z1/F");
+  ktrack -> Branch ("px1",       &px1,       "px1/F");
+  ktrack -> Branch ("py1",       &py1,       "py1/F");
+  ktrack -> Branch ("pz1",       &pz1,       "pz1/F");
+  ktrack -> Branch ("x3",        &x3,        "x3/F");
+  ktrack -> Branch ("y3",        &y3,        "y3/F");
+  ktrack -> Branch ("z3",        &z3,        "z3/F");
+  ktrack -> Branch ("px3",       &px3,       "px3/F");
+  ktrack -> Branch ("py3",       &py3,       "py3/F");
+  ktrack -> Branch ("pz3",       &pz3,       "pz3/F");
+  ktrack -> Branch ("tx_PT",     &tx_PT,     "tx_PT/F");
+  ktrack -> Branch ("ty_PT",     &ty_PT,     "ty_PT/F");
+  ktrack -> Branch ("targetPos", &targetPos, "targetPos/I");
+  ktrack -> Branch ("target",    &target,    "target/I");
+  ktrack -> Branch ("dump",      &dump,      "dump/I");
+
+  treeql -> SetBranchAddress ("runID",     &runID);
+  treeql -> SetBranchAddress ("spillID",   &spillID);
+  treeql -> SetBranchAddress ("eventID",   &eventID);
+  treeql -> SetBranchAddress ("charge",    &charge);
+  treeql -> SetBranchAddress ("roadID",    &roadID);
+  treeql -> SetBranchAddress ("chisq",     &chisq);
+  treeql -> SetBranchAddress ("x0",        &x0);
+  treeql -> SetBranchAddress ("y0",        &y0);
+  treeql -> SetBranchAddress ("z0",        &z0);
+  treeql -> SetBranchAddress ("x_target",  &x_target);
+  treeql -> SetBranchAddress ("y_target",  &y_target);
+  treeql -> SetBranchAddress ("z_target",  &z_target);
+  treeql -> SetBranchAddress ("x_dump",    &x_dump);
+  treeql -> SetBranchAddress ("y_dump",    &y_dump);
+  treeql -> SetBranchAddress ("z_dump",    &z_dump);
+  treeql -> SetBranchAddress ("x1",        &x1);
+  treeql -> SetBranchAddress ("y1",        &y1);
+  treeql -> SetBranchAddress ("z1",        &z1);
+  treeql -> SetBranchAddress ("px1",       &px1);
+  treeql -> SetBranchAddress ("py1",       &py1);
+  treeql -> SetBranchAddress ("pz1",       &pz1);
+  treeql -> SetBranchAddress ("x3",        &x3);
+  treeql -> SetBranchAddress ("y3",        &y3);
+  treeql -> SetBranchAddress ("z3",        &z3);
+  treeql -> SetBranchAddress ("px3",       &px3);
+  treeql -> SetBranchAddress ("py3",       &py3);
+  treeql -> SetBranchAddress ("pz3",       &pz3);
+  treeql -> SetBranchAddress ("tx_PT",     &tx_PT);
+  treeql -> SetBranchAddress ("ty_PT",     &ty_PT);
+  //treeql -> SetBranchAddress ("targetPos", &targetPos);
+  treeql -> SetBranchAddress ("target",    &target);
+  treeql -> SetBranchAddress ("dump",      &dump);
+  
+  int nentries = treeql -> GetEntries();
+  cout << "The number of Entries is " << nentries << endl;
+  for(int i = 0; i < nentries; i++){
+    treeql -> GetEntry(i);
+    ktrack -> Fill();
+  }
+
+
+  char outname[128];
+  sprintf(outname, "./nDST/%s_%d.root", schema, val);
+  TFile *outfile = new TFile(outname,"RECREATE");
+  outfile -> cd();
+  ktrack -> Write();
+  outfile -> Close();
+  
+  // Take out the garbage on your way out, and delete the created table.
+  delete treeql;
+  sprintf(stmt,"DROP TABLE IF EXISTS ktrack_%d", val); 
+  con->Exec(stmt);
+  
+  // Disconnect from the server
+  delete con;
+  
+
+
+}
