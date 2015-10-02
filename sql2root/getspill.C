d99ff599ff099d19fe7f0c00225b2221870b55b4
David Kleinjan <kleinjan@lanl.gov>
2015-10-02 17:58:45 -0600

    getspill done

--- sql2root/getspill.C
+++ sql2root/getspill.C
@@ -15,29 +15,32 @@ void getspill(const char *schema)
   UInt_t runID;
   Int_t spillID;
   Float_t liveProton;
+  Float_t liveProton2;
 
   TTree *spill = new TTree("spill","spill");
   spill -> SetDirectory(0);
   spill -> Branch ("runID",           &runID,           "runID/I");
   spill -> Branch ("spillID",         &spillID,         "spillID/I");
   spill -> Branch ("liveProton",      &liveProton,      "liveProton/F");
+  spill -> Branch ("liveProton2",      &liveProton2,      "liveProton2/F");
 
   treeql -> SetBranchAddress ("runID",           &runID);
   treeql -> SetBranchAddress ("spillID",         &spillID);
   treeql -> SetBranchAddress ("liveProton",      &liveProton);
+  treeql -> SetBranchAddress ("liveProton2",      &liveProton2);
 
   
   int nentries = treeql -> GetEntries();
   cout << "The number of Entries is " << nentries << endl;
   for(int i = 0; i < nentries; i++){
-    if(i%100000 == 0) cout << i << endl;
+    if(i%10000 == 0) cout << i << endl;
 
     treeql -> GetEntry(i);
-    cout<< i << " " << runID << " " << spillID << " " << liveProton << endl;
+    cout<< i << " " << runID << " " << spillID << " " << liveProton << " " << liveProton2 << endl;
     spill -> Fill();
 
   }
-  
+ cout<<"blah"<<endl; 
   char outname[128];
   sprintf(outname, "./nDST/%s_spill.root", schema);
   TFile *outfile = new TFile(outname,"RECREATE");
