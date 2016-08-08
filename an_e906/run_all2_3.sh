9f09d5dc39767480bbd02d8a75bf7870e099a159
David Kleinjan <kleinjan@lanl.gov>
2016-08-08 09:53:32 -0600

    code of systematic A_N E1039

---
+++ an_e906/run_all2_3.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo -n "Enter the first number --> "
+read roadset
+echo -n "The roadset it ${roadset}"
+
+#echo "root.exe asym2_halfday.C\(0,1,${roadset},0,1\)";
+#root.exe -b -q asym2_halfday.C\(0,1,${roadset},0,1\)
+#echo "root.exe asym2_halfday.C\(0,5,${roadset},0,1\)";
+#root.exe -b -q asym2_halfday.C\(0,5,${roadset},0,1\)
+for((i = 3001; i < 4001; i++))
+do
+echo "$i"
+echo "root.exe asym2_halfday.C\(0,1,${roadset},$i\)";
+root.exe -b -q asym2_halfday.C\(0,1,${roadset},$i\)
+echo "root.exe asym2_halfday.C\(0,5,${roadset},$i\)";
+root.exe -b -q asym2_halfday.C\(0,5,${roadset},$i\)
+
+done
--- an_e906/run_all_bs.sh
+++ an_e906/run_all_bs.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
-roadset=57
-#echo "root.exe asym2_bs.C\(0,1,${roadset},0,1\)";
-#root.exe -b -q asym2_bs.C\(0,1,${roadset},0,1\)
-for((i = 1; i < 1001; i++))
+roadset=67
+echo "root.exe asym2_bs.C\(0,1,${roadset},0,1\)";
+root.exe -b -q asym2_bs.C\(0,1,${roadset},0,1\)
+echo "root.exe asym2_bs.C\(0,5,${roadset},0,1\)";
+root.exe -b -q asym2_bs.C\(0,5,${roadset},0,1\)
+for((i = 1; i < 5001; i++))
 do
 echo "$i"
 echo "root.exe asym2_bs.C\(0,1,${roadset},$i\)";
 root.exe -b -q asym2_bs.C\(0,1,${roadset},$i\)
+echo "root.exe asym2_bs.C\(0,5,${roadset},$i\)";
+root.exe -b -q asym2_bs.C\(0,5,${roadset},$i\)
 
 done
