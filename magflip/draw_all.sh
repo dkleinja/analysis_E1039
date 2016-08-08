7f48bd0d92f2957754d6c3614de87e6ed5680610
dkleinja <kleinjan@lanl.gov>
2016-08-08 10:18:54 -0600

    some macro code

---
+++ magflip/draw_all.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+
+root.exe -b -q drawmagflip.C\(4,57,59\)
+root.exe -b -q drawmagflip.C\(4,57,62\)
+root.exe -b -q drawmagflip.C\(4,57,67\)
+root.exe -b -q drawmagflip.C\(4,57,70\)
+
+root.exe -b -q drawmagflip.C\(4,59,62\)
+root.exe -b -q drawmagflip.C\(4,59,67\)
+root.exe -b -q drawmagflip.C\(4,59,70\)
+
+root.exe -b -q drawmagflip.C\(4,62,67\)
+root.exe -b -q drawmagflip.C\(4,62,70\)
+
+root.exe -b -q drawmagflip.C\(4,68,69\)
+root.exe -b -q drawmagflip.C\(4,67,70\)
