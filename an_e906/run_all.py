ad68f91451fbbcea1a373e3ebcbfffed0a9afae3
dkleinja <kleinjan@lanl.gov>
2016-08-16 19:12:28 -0500

    should speed up submission

--- an_e906/run_all.py
+++ an_e906/run_all.py
@@ -3,15 +3,15 @@
 import os
 
 #Command Line Arguments are stored in
-nCpus = 9;
+nCpus = 10;
     
 for jobno in range(0, nCpus):
     print '\t This is job number %d' % jobno;
     #print '\n';
-    runstart = 500*jobno + 1
+    runstart = 1000*jobno + 5001
     shelljob = "./run_all.sh 67 %d " % runstart
     print shelljob;
-    os.system(shelljob)
+    #os.system(shelljob)
 print 'Done with submitting %d jobs' % nCpus
 
 os.system("echo finalstatement");
